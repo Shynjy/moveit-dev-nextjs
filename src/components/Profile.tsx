ff5175298ca07b8eafb567b721ca066b0452f6ed
Shynjy <furia.designer@gmail.com>
2021-02-26 17:19:47 -0300

    update responsividade

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,16 +4,18 @@ import style from '../styles/components/Profile.module.scss'
 
 export default function Profile() {
     const { level } = useContext(ChallengesContext)
- 
+
     return (
         <div className={style.profileContainer}>
-            <img src="https://github.com/Shynjy.png" alt="Foto do perfil" />
-            <div>
-                <strong>David Pereira</strong>
-                <p>
-                    <img src="icons/level.svg" alt="Level"/>
+            <div className={style.boxProfile}>
+                <img src="https://github.com/Shynjy.png" alt="Foto do perfil" />
+                <div>
+                    <strong>David Pereira</strong>
+                    <p>
+                        <img src="icons/level.svg" alt="Level" />
                     Level {level}
                     </p>
+                </div>
             </div>
         </div>
     );
